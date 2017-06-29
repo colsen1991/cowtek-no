2ba2e00ee93e9765224e3ab079c62fb2d0a9c5ff
Christer Olsen <colsen1991@gmail.com>
2017-06-29 11:35:30 +1000

    A few adjustments to the header logo. Menu button turns into 'X' when showing menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,15 +2,18 @@ function toggleMenu () {
   const main = document.getElementById('main')
   const menu = document.getElementById('menu-content')
   const footer = document.getElementById('footer')
+  const menuButton = document.getElementById('menu-button')
 
   if (main.className.includes('hide')) {
     main.className = 'show'
     footer.className = 'show'
     menu.className = 'hide'
+    menuButton.className = 'fa fa-bars'
   } else {
     main.className = 'hide'
     footer.className = 'hide'
     menu.className = 'show'
+    menuButton.className = 'fa fa-times'
   }
 }
 
