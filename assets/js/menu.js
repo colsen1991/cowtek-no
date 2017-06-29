9446e4f44e61bd2c25ba12c796db4845faa6183a
Christer Olsen <colsen1991@gmail.com>
2017-06-29 17:11:07 +1000

    nav menu items tabindex toggle

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,20 +1,28 @@
-function toggleMenu () {
-  const main = document.getElementById('main')
-  const menu = document.getElementById('menu-content')
-  const footer = document.getElementById('footer')
-  const menuButton = document.getElementById('menu-button')
-
-  if (main.className.includes('hide')) {
-    main.className = 'show'
-    footer.className = 'show'
-    menu.className = 'hide'
-    menuButton.className = 'fa fa-bars'
-  } else {
-    main.className = 'hide'
-    footer.className = 'hide'
-    menu.className = 'show'
-    menuButton.className = 'fa fa-times'
+function setTabindexElements (elements, index) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].setAttribute('tabindex', index)
   }
+}
+
+function showHideMenu (elements, show) {
+  elements.main.className = show ? 'hide' : 'show'
+  elements.footer.className = show ? 'hide' : 'show'
+  elements.menu.className = show ? 'show' : 'hide'
+  elements.menuButton.className = show ? 'fa fa-times' : 'fa fa-bars'
+}
+
+function toggleMenu () {
+  const elements = {
+    main: document.getElementById('main'),
+    menu: document.getElementById('menu-content'),
+    footer: document.getElementById('footer'),
+    menuButton: document.getElementById('menu-button')
+  }
+  const navLinks = document.getElementsByClassName('nav-link')
+  const showMenu = elements.menu.className.includes('hide')
+
+  showHideMenu(elements, showMenu)
+  setTabindexElements(navLinks, showMenu ? '0' : '-1')
 }
 
 document.getElementById('menu-button').addEventListener('click', toggleMenu)
